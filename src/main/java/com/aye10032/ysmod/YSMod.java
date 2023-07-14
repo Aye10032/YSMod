ac6f594cf0988211c26d86a6a329cb6bc13f5d23
Aye10032 <aye10032@gmail.com>
2023-07-14 13:02:59 +0800

    change mod id

--- src/main/java/com/aye10032/ysmod/YSMod.java
+++ src/main/java/com/aye10032/ysmod/YSMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("YSMod")
+@Mod("ysmod")
 public class YSMod {
 
     // Directly reference a slf4j logger
